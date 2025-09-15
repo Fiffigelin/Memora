d0f9434d5086fe6e039c85c7a64f79e129c5e9f1
Elin Nyman <elin.nyman87@gmail.com>
2025-09-15 16:58:29 +0200

    fix: styling --not finished

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import AppRoutes from "./routes/app-routes.tsx";
 import { AuthProvider } from "./context/auth-context.tsx";
+
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- Frontend/src/pages/public/login-page.tsx
+++ Frontend/src/pages/public/login-page.tsx
@@ -56,48 +56,46 @@ export default function LoginPage() {
   }
 
   return (
-    <section className="login-page">
-      <div className="login-container">
-        <div className="card">
-          <div className="card-title">
-            <h1>Login</h1>
-            <div className="card-text">
-              <p>Doesn't have a account yet?</p>
-              <p className="uri">Sign up</p>
-            </div>
+    <section className="login-container">
+      <div className="card">
+        <div className="card-title">
+          <h1>Login</h1>
+          <div className="card-text">
+            <p>Doesn't have a account yet?</p>
+            <p className="uri">Sign up</p>
           </div>
-          <div className="card-content">
-            <div className="card-input">
-              <ValidationInput
-                label="Email"
-                validationmsg={"Inkorrekt email address"}
-                placeholder="Skriv din email"
-                value={user?.email ?? ""}
-                onChange={(value) => {
-                  updateLoginEmail(value);
-                }}
-                isValid={validLoginEmail}
-              />
-              <ValidationInput
-                label="Lösenord"
-                validationmsg={"Inkorrekt email address"}
-                placeholder="Skriv ditt lösenord"
-                value={user?.password ?? ""}
-                onChange={(value) => {
-                  updatePassword(value);
-                }}
-                isValid={true}
-                type={"password"}
-              />
-            </div>
-            <div className="card-button">
-              <CommonButton
-                onClick={validateLogin}
-                title="LOGIN"
-                variant="default"
-                disabled={false}
-              />
-            </div>
+        </div>
+        <div className="card-content">
+          <div className="card-input">
+            <ValidationInput
+              label="Email"
+              validationmsg={"Inkorrekt email address"}
+              placeholder="Skriv din email"
+              value={user?.email ?? ""}
+              onChange={(value) => {
+                updateLoginEmail(value);
+              }}
+              isValid={validLoginEmail}
+            />
+            <ValidationInput
+              label="Lösenord"
+              validationmsg={"Inkorrekt email address"}
+              placeholder="Skriv ditt lösenord"
+              value={user?.password ?? ""}
+              onChange={(value) => {
+                updatePassword(value);
+              }}
+              isValid={true}
+              type={"password"}
+            />
+          </div>
+          <div className="card-button">
+            <CommonButton
+              onClick={validateLogin}
+              title="LOGIN"
+              variant="default"
+              disabled={false}
+            />
           </div>
         </div>
       </div>
