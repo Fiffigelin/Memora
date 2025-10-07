b1fd33e13befc4983d7978c7e6cd1daff8b0039f
Elin Nyman <elin.nyman87@gmail.com>
2025-10-07 13:00:56 +0200

    fix: eslint error

--- Frontend/src/utils/validations.ts
+++ Frontend/src/utils/validations.ts
@@ -1,6 +1,4 @@
-const emailRegex = new RegExp(
-  /^(("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+\/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))((\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,2}[a-z0-9]))$/
-);
+const emailRegex =
+  /^(("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#%&'*+/=?^`{}|~\w])*)(?<=[0-9a-z])@))((\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][-a-z0-9]{0,2}[a-z0-9]))$/;
 
-export const isEmail = (input: string | undefined): boolean =>
-  !!input && emailRegex.test(input);
+export const isEmail = (input: string | undefined): boolean => !!input && emailRegex.test(input);
