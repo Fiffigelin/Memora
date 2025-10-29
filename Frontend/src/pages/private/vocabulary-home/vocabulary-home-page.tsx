98aa5aea8ff1eff1fd9d3c1f9efa1ecb708e121c
Elin Nyman <elin.nyman87@gmail.com>
2025-10-29 13:45:13 +0100

    fix: remove package and created custom grid-system for vocabulary cards

--- Frontend/src/pages/private/vocabulary-home/vocabulary-home-page.tsx
+++ Frontend/src/pages/private/vocabulary-home/vocabulary-home-page.tsx
@@ -1,9 +1,48 @@
+import CommonButton from "../../../components/common-button/common-button";
+import VocabCard from "../../../components/vocab-card/vocab-card";
+
 import "./vocabulary-home-page.scss";
+import { useOutletContext } from "react-router-dom";
+import { UserProfileDto } from "../../../api/client";
+
+type DashboardContext = { user: UserProfileDto };
 
 export default function VocabularyHome() {
+  const { user } = useOutletContext<DashboardContext>();
+
+  const list1 = user?.vocabularyLists || [];
+  const list2 = user?.vocabularyLists || [];
+  const list3 = user?.vocabularyLists || [];
+
+  const testLists = [...list1, ...list2, ...list3];
+
   return (
-    <div className="vocabulary">
-      <p>Vocabulary!</p>
+    <div className="vocabulary-home-container">
+      <div className="vocabulary-header">
+        <h1>Glosor</h1>
+        <CommonButton
+          title="Skapa ny gloslista"
+          variant="default"
+          onClick={() => {
+            console.log("Skapa ny lista");
+          }}
+        />
+      </div>
+      <div className="vocabulary-content">
+        <div className="vocabulary-list">
+          {/* {user?.vocabularyLists?.map((item) => ( */}
+          {testLists.map((item) => (
+            <VocabCard
+              key={item.id}
+              id={item.id!}
+              createdAt={item.createdAt}
+              language={item.language!}
+              title={item.title!}
+              amountOfWords={item.vocabularies ? item.vocabularies.length : 0}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
